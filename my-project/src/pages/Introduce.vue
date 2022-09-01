1140dbbabd1bdc3b6cd9d6b9061a1d976f1483a4
DESKTOP-F2VQJ5V\ghkds <ghkdsigm3@gmail.com>
2022-09-01 18:15:27 +0900

    소개페이지 업데이트

--- my-project/src/pages/Introduce.vue
+++ my-project/src/pages/Introduce.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    안냐세욤 도키에용
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col lg:flex-row">
+        <img class="w-full rounded-lg shadow-2xl" src="https://img.freepik.com/premium-photo/business-people-meeting-discussion-corporate-handshake-concept_53876-69152.jpg?w=1800" />
+        <div class="mt-10">
+          <h1 class="text-5xl font-bold">DOKI는,</h1>
+          <p class="py-6">웹 세상을 창조하는 아름다움으로 새로운 역사를 써 내려갑니다. 온라인 공간에서 사람과 세상을 더 아름답게 하는 소명을 이루고자 미의 여정을 이어갑니다.</p>         
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
