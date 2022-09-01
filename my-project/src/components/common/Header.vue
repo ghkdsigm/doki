292f8c8d49107eab05b56be2f69e24f92d0148de
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-09-01 20:21:54 +0900

    css수정

--- my-project/src/components/common/Header.vue
+++ my-project/src/components/common/Header.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <div class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
       <div class="flex flex-wrap justify-between items-center lg:container mx-auto">
         <router-link to="/" class="flex items-center">
             <img class="h-8 w-auto sm:h-10 mr-4" src="https://flowbite.com/docs/images/logo.svg" alt="">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">DOKI</span>
+            <span class="self-center text-xl font-semibold whitespace-nowrap">DOKI</span>
         </router-link>        
         <div class="flex items-center lg:order-2">
-            <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
-            <router-link to="/modelselect" class="bg-blue-700 text-white hover:text-white hover:bg-blue-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none dark:focus:ring-gray-800">간편 견적내기</router-link>
+            <a href="#" class="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ">Log in</a>
+            <router-link to="/modelselect" class="bg-blue-700 text-white hover:text-white hover:bg-blue-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ">간편 견적내기</router-link>
         </div>
         <div class="justify-between items-center w-full flex lg:w-auto lg:order-1">
             <router-link :to="route.path" class="lg:px-4 lg:text-lg text-sm" v-for="route in routes" :key="route"> 
