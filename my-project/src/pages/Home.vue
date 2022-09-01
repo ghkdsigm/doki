efe583062a7427bbaec45513168012dc44b2d05a
ghkdsigm3 <ghkdsigm3@gmail.com>
2022-09-01 20:56:05 +0900

    artcolor 반영

--- my-project/src/pages/Home.vue
+++ my-project/src/pages/Home.vue
@@ -6,8 +6,8 @@
         <div class="relative mx-auto sm:static">
           <div class="sm:max-w-lg">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-              <span class="block xl:inline">Show the work for your</span>
-              <span class="block text-indigo-600 xl:inline">online business</span> 
+              <span class="block">Show the art of web site for your</span>
+              <span class="block artcolor xl:inline">Online business</span> 
             </h1>
             <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">쉽고 빠르게 합리적인 비용으로 다양한 테마의 퍼블리싱 웹사이트를 구매하실 수 있습니다.</p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
@@ -64,7 +64,7 @@
     <div class="">
       <div class="">
         <div class="mx-auto max-w-2xl py-12 sm:py-12 lg:max-w-none lg:py-20">
-          <h2 class="text-2xl font-bold text-gray-900">Collections</h2>
+          <h2 class="text-2xl font-bold text-gray-900">Services</h2>
 
           <div class="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             <div class="group relative">
--- my-project/src/pages/Introduce.vue
+++ my-project/src/pages/Introduce.vue
@@ -3,9 +3,13 @@
     <div class="hero min-h-screen bg-base-200">
       <div class="hero-content flex-col lg:flex-row">
         <img class="w-full rounded-lg shadow-2xl" src="https://img.freepik.com/premium-photo/business-people-meeting-discussion-corporate-handshake-concept_53876-69152.jpg?w=1800" />
-        <div class="mt-10">
-          <h1 class="text-5xl font-bold">DOKI는,</h1>
-          <p class="py-6">웹 세상을 창조하는 아름다움으로 새로운 역사를 써 내려갑니다. 온라인 공간에서 사람과 세상을 더 아름답게 하는 소명을 이루고자 미의 여정을 이어갑니다.</p>         
+        <div class="lg:mt-12 lg:mb-40 mt-10 mb-32">
+          <h1 class="text-5xl font-bold"><span class="artcolor">DOKI</span>는,</h1>
+          <div class="py-5">
+            <p class="py-3 font-medium text-xl">웹 세상을 창조하는 아름다움으로 새로운 역사를 써 내려갑니다.<br/> 온라인 공간에서 사람과 세상을 더 아름답게 하는 소명을 이루고자 미의 여정을 이어갑니다.</p>  
+            <p class="py-3 text-lg"><span class="text-blue-700 font-bold text-xl">첫째,</span> 도키의 웹퍼블리싱 작업물 구매 서비스는 옵션별 선택(간편견적) 시스템을 통해 손쉽게 선택하고 이용하실 수 있습니다.</p>  
+            <p class="py-3 text-lg"><span class="text-blue-700 font-bold text-xl">둘째,</span> 다양한 계층, 직군, 분야에서 활용될 테마별 퍼블리싱 작업물을 저렴한 비용으로 구입하실 수 있습니다.</p>              
+          </div>
         </div>
       </div>
     </div>
